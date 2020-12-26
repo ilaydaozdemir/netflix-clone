7e9793148a60dfb0c84eb6dbb703097148b8854c
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-27 00:15:38 +0300

    request işlemi

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import axios from '../axios';
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     //if [],run once when the Row loads,and do not run again
-  }, [movies]); //if movies chance during run
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //https://api.themovie.org/3
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <h2>{title}</h2>

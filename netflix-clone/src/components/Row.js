57bf4857e750ec1754d17c162fe2d0e6c3a555fa
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-27 15:42:59 +0300

    data çekip css duzenlemesı yapıldı

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../axios';
+import '../css/Row.css';
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
@@ -7,14 +10,25 @@ function Row({ title, fetchUrl }) {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       //https://api.themovie.org/3
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.table(movies);
   return (
-    <div>
+    <div className='row'>
       <h2>{title}</h2>
+      <div className='row__posters'>
+        {movies.map(movie => (
+          <img
+            className='row__poster'
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }

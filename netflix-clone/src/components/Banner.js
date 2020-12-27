bbfc3620ed4e4c6ea1eca10f27ce84331605c748
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-27 17:41:08 +0300

    adding description

--- netflix-clone/src/components/Banner.js
+++ netflix-clone/src/components/Banner.js
@@ -29,12 +29,15 @@ function Banner() {
         {/* <<<Background image*/}
         {/* title*/}
         <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+
+        {/* div>2 buttons*/}
         <div className='banner__buttons'>
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
-        {/* div>2 buttons*/}
+
         {/* description*/}
+        <h1 className='banner__description'>{movie?.overview}</h1>
       </div>
     </header>
   );

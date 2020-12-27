548e1e18b125af2723c36638ce96b8282245fd4e
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-27 17:54:52 +0300

    Banner css

--- netflix-clone/src/components/Banner.js
+++ netflix-clone/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../axios';
 import requests from '../requests';
+import '../css/Banner.css';
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -25,10 +26,12 @@ function Banner() {
         backgroundPosition: 'center center',
       }}
     >
-      <div className='banner_contents'>
+      <div className='banner__contents'>
         {/* <<<Background image*/}
         {/* title*/}
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
 
         {/* div>2 buttons*/}
         <div className='banner__buttons'>
